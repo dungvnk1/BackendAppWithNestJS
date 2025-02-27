241b599c7ebf1279bd5daa6574c67dc92e42579d
dungvnk1 <vunangdungdta@gmail.com>
2025-02-27 11:19:24 +0700

    Fix code

--- src/migrations/1740590563012-CreateCustomerTable.ts
+++ src/migrations/1740590563012-CreateCustomerTable.ts
@@ -1,14 +1,38 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateCustomerTable1740590563012 implements MigrationInterface {
     name = 'CreateCustomerTable1740590563012'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "customer" ("id" SERIAL NOT NULL, "username" character varying NOT NULL, "password" character varying NOT NULL, "email" character varying NOT NULL, CONSTRAINT "UQ_cb485a32c0e8b9819c08c1b1a1b" UNIQUE ("username"), CONSTRAINT "PK_a7a13f4cacb744524e44dfdad32" PRIMARY KEY ("id"))`);
+        await queryRunner.createTable(new Table({
+            name: 'customer',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+                },
+                {
+                    name: 'username',
+                    type: 'varchar',
+                    isUnique: true,
+                },
+                {
+                    name: 'password',
+                    type: 'varchar',
+                },
+                {
+                    name: 'email',
+                    type: 'varchar',
+                },
+            ],
+        }), true);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "customer"`);
+        await queryRunner.dropTable('customer');
     }
 
 }
